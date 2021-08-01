b1c5fa5f0a4a2272e1858bb7f83dc1f303d7df59
lyh71709 <60583161+lyh71709@users.noreply.github.com>
2021-08-02 10:47:52 +1200

    Current

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+    
+ini_set('display_errors', 'On'); //Change to 'Off' when live
+
+define("DB_NAME","lyh71709_L2_Games_DB");
+define("DB_USERNAME","_lyh71709");
+define("DB_PASSWORD", "ephCfFopNqDPjDpE");
+define("DB_HOST","localhost");
+    
+?>
