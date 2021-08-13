4916545d3693e1f014acfe295b2ecc5cc18ea81f
lyh71709 <60583161+lyh71709@users.noreply.github.com>
2021-08-13 12:30:11 +1200

    Current

--- showall.php
+++ showall.php
@@ -10,33 +10,33 @@
 
 ?>
             
-        <div class="box main">
+    <div class="box main">
             <h2>All Results</h2>
-            
+
             <?php
-                
+
             if($count < 1) {
-                
+
                 ?>
-                    
+
             <div class="error">
-                    
+
                     Sorry! There are no results that match your search. 
                     Please use the search box in the side bar to try again.
-                        
+
             </div> <!-- end error -->
-                    
+
             <?php
             } // end no results if
-            
+
             else {
                 do
                 {
                     ?>
-            
+
             <!-- Results go here -->
             <div class="results">
-                
+
                 <!-- Heading and Subtitle -->
                 <div class="flex-container">
                     <div>
@@ -45,7 +45,7 @@
                             </a>
                         </span>
                     </div> <!-- / Title -->
-                    
+
                     <?php
                         if($find_rs['Subtitle'] != "") 
                         { 
@@ -57,71 +57,59 @@
                     <?php
                         }
                     ?>
-                
+
                 </div>
                 <!-- / Heading and Subtitle -->
-                
-                <p>
-                
+
+                <!-- Rating Area -->
+
+                <div class="flex-container">
+                    <div class="star-rating-sprite">
+                        <span style="width:<?php echo $find_rs['User Rating'] * 20; ?>%" class="star-ratings-sprite-rating"></span>
+                    </div> <!-- / star rating div -->
+
+                    <div class="actual-rating">
+                        (<?php echo $find_rs['User Rating'] ?> based on <?php echo number_format($find_rs['Rating Count']) ?> ratings)
+                    </div> <!-- / text rating div -->
+                </div> <!-- / ratings flexbox -->
+
                 <!-- Price -->
                 
-                <b>Price</b>:
                 <?php 
-                    if(echo $find_rs['Price'] == 0
-                    {
-                    ?>
-                    
-                    <p>Price: Free 
+
+                    if( $find_rs['Price'] == 0) {
+                        ?>
+
+                    <p>
+                        <b>Price:</b> Free
                         <?php
                             if($find_rs['In App'] == 1)
                             {
                                 ?>
-                                (In App Purchases)
+                                    (In App Purchases)
                                 <?php
-                            }// end In App if
-                    
-                <?php
+                            } // end In App if
+                        ?>
+                    </p>
+
+                    <?php
                     } // end price if
-                       
-                       else {
-                           
-                           ?>
-                        <b>Price:</b> $<?php echo $find_rs['Price'] ?>
-                           
-                        <?php
-                        } // end price else (displays cost)
-                    ?>
-                
-                
-                
-                
-                
-                
-                </p>
-                
-                
-                <!--<p>
-                    <b>Genre</b>:
-                    <?php echo $find_rs['Genre']?>
-                    
-                    <br/>
-                    
-                    <b>Developer</b>:
-                    <?php echo $find_rs['Developer']?>
-                    
-                    <br/>
-                    
-                    <b>Rating</b>:
-                    <?php echo $find_rs['User Rating']; ?>
-                    (Based on <?php echo $find_rs['Rating Count']; ?> votes)
-                
-                </p> -->
-                
-                <hr />
-                <?php echo $find_rs['Description']?>
+
+                    else {
+                               ?>
+                            <p>
+                             <b>Price:</b> $<?php echo $find_rs['Price'] ?>
+                            </p>
+
+                            <?php
+                            } // end price else (displays cost)
+                        ?>
+                <!-- / Price -->
+
             </div> <!-- / results -->
             
             <br />
+        
             <?php
                 } // end results 'do'
                 
@@ -131,8 +119,7 @@
             
             ?>
             
-
-            
         </div> <!-- / main -->
         
 <?php include("bottombit.php") ?>
+            
